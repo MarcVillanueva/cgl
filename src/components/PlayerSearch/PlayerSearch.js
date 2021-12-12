8bc988342da076dc53e9eaf4c63a198cc5fad89c
Marc <marc.villanueva@mitchell.com>
2021-12-12 15:23:00 -0800

    Add PlayerSearch component template

---
+++ src/components/PlayerSearch/PlayerSearch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const PlayerSearch = () => (
+  <div >
+    PlayerSearch Component
+  </div>
+);
+
+// PlayerSearch.propTypes = {};
+
+// PlayerSearch.defaultProps = {};
+
+export default PlayerSearch;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import PlayerSearch from "./components/PlayerSearch/PlayerSearch";
 
 function Greeting(){
-  return <h4>This is John and this is my first component</h4>
+  return <PlayerSearch></PlayerSearch>
+  // return <h4>This is John and this is my first component</h4>
 }
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
