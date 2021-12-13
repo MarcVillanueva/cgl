79b460341547ace04bab4aafc4fec5227df6bdac
Marc <marc.villanueva@mitchell.com>
2021-12-13 14:22:34 -0800

    Fetch default user data when the App component is mounted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 import PlayerSearch from "./components/PlayerSearch/PlayerSearch";
 
-    class App extends Component {
-      render() {
-        return (
-          <PlayerSearch></PlayerSearch>
-        );
-      }
+class App extends Component {
+    state = {
+        playerInfo: []
     }
 
-    export default App;
+    componentDidMount() {
+        console.log('componentDidMount() lifecycle');
+        fetch('https://api.sleeper.app/v1/user/marcvillanueva')
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({ playerInfo: data })
+            console.log(data)
+        })
+        .catch(console.log)
+    }
+
+    render() {
+    return (
+        <PlayerSearch></PlayerSearch>
+    );
+    }
+
+
+}
+
+export default App;
