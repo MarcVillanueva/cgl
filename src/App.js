4f8567fe42c0ff4fd95506122848c79f1ae04f72
Marc <marc.villanueva@mitchell.com>
2021-12-13 20:52:58 -0800

    Remove logic to fetch user data in App component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,6 @@ class App extends Component {
         playerInfo: []
     }
 
-    componentDidMount() {
-        console.log('componentDidMount() lifecycle');
-        fetch('https://api.sleeper.app/v1/user/marcvillanueva')
-        .then(res => res.json())
-        .then((data) => {
-            this.setState({ playerInfo: data })
-            console.log(data)
-        })
-        .catch(console.log)
-    }
-
     render() {
     return (
         <FormPlayerSearch></FormPlayerSearch>
