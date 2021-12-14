fbb5313dd68e522d504e3850906de32cbc6dcd99
Marc <marc.villanueva@mitchell.com>
2021-12-13 19:55:05 -0800

    FormPlayerSearch: Create username state and bind it the value of the input

--- src/components/FormPlayerSearch/FormPlayerSearch.js
+++ src/components/FormPlayerSearch/FormPlayerSearch.js
@@ -3,17 +3,27 @@ import React from 'react';
 export default class FormPlayerSearch extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+    username: ""
+  }
   }
 
-    render() {
+  render() {
     return  (
       <div>
-        <input type="text" />
+        <input type="text" value={this.state.username} onChange={this.handleInputChanged.bind(this)}/>
         <button>
           Submit
         </button>
       </div>
     );
+  }
+
+  handleInputChanged(event) {
+    this.setState({
+      username: event.target.value
+    });
   }
 
 }
