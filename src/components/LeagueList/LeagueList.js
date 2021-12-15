600d10d1e87a201b3df58fd7debf6f4a3e2f5592
Marc <marc.villanueva@mitchell.com>
2021-12-15 08:50:21 -0800

    Create placeholder LeagueList component

---
+++ src/components/LeagueList/LeagueList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './LeagueList.module.css';
+
+const LeagueList = () => (
+  <div className={styles.LeagueList}>
+    LeagueList Component
+  </div>
+);
+
+LeagueList.propTypes = {};
+
+LeagueList.defaultProps = {};
+
+export default LeagueList;
