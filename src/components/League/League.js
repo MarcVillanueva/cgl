22177cdef9313ea0f31cde02feb0477cf68fbdc6
Marc <marc.villanueva@mitchell.com>
2021-12-15 08:51:18 -0800

    Add placeholder League component

---
+++ src/components/League/League.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './League.module.css';
+
+const League = () => (
+  <div className={styles.League}>
+    League Component
+  </div>
+);
+
+League.propTypes = {};
+
+League.defaultProps = {};
+
+export default League;
