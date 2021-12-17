36f003b19f597924596615dd32a1b5c6dd0a44d8
Marc <marc.villanueva@mitchell.com>
2021-12-16 16:14:47 -0800

    Fix warnings: Unused imports

--- src/components/LeagueList/LeagueList.js
+++ src/components/LeagueList/LeagueList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import League from '../League/League';
 
 const LeagueList = (props) => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import App from './App'
 import UserInformation from './UserInformation'
 
