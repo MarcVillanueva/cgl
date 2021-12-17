a996aad3316102f47082486e876be3a396e1feb9
Marc <marc.villanueva@mitchell.com>
2021-12-17 10:49:32 -0800

    Migrate more pre-existing SearchBar functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,26 +1,64 @@
 import React from 'react'
 import './SearchBar.css'
 import SearchIcon from '@material-ui/icons/Search'
+import { useNavigate } from "react-router-dom"
 
-const SearchBar = () => (
-      <div>
-        <div className="navbar-container">
-          <div className="navbar">
-            <div className="logo-section">Sleeper</div>
-            <div className="searchbar-section">
-              <input id="searchbar" type="text" autoComplete="off"/>
-              <div className="searchbar-placeholder" onClick={(e) => {
-                document.getElementById("searchbar")?.focus()
-                }}>
-                <SearchIcon id="Searchbar-placeholder-icon"></SearchIcon>
-                <span>Search</span>
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      username: ""
+    }
+  }
+
+  render() {
+    return (
+        <div>
+          <div className="navbar-container">
+            <div className="navbar">
+              <div className="logo-section">Sleeper</div>
+              <div className="searchbar-section">
+                <input id="searchbar" type="text" autoComplete="off" value={this.state.username} onChange={this.handleInputChanged.bind(this)} onKeyDown={this.handleKeyDown.bind(this)}/>
+                {
+                  this.state.username === "" && (
+                
+                <div className="searchbar-placeholder" onClick={(e) => {
+                  document.getElementById("searchbar")?.focus()
+                  }}>
+                  <SearchIcon id="Searchbar-placeholder-icon"></SearchIcon>
+                  <span>Search</span>
+                </div>
+                  )
+                }
               </div>
+              <div className="actions-section">Actions</div>
             </div>
-            <div className="actions-section">Actions</div>
           </div>
         </div>
-      </div>
-);
+    )
+  }
+
+  handleInputChanged(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+
+  async handleSubmit() {
+    var username = this.state.username;
+    this.props.navigation("/user/" + username + "/");
+  }
+
+  handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      this.handleSubmit();
+    }
+  }
+}
+
+
+
 
 SearchBar.propTypes = {};
 
