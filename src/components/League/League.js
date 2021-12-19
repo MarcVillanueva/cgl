a60a59364f4c6f0476846e221f9d1949667a0b15
Marc <marc.villanueva@mitchell.com>
2021-12-19 14:11:34 -0800

    Wrap League component inside anchor tag - Making League component clickable - TODO: Create a LeagueInformation component to route to when a league is clicked on

--- src/components/League/League.js
+++ src/components/League/League.js
@@ -3,14 +3,16 @@ import "./League.css"
 import DefaultLogo from '../../assets/default_league_logo.png'
 
 const League = (props) => (
-  <div className="league">
-    <label className="league-name">{props.name}</label>
-    <img 
-    src={props.avatar === null ? DefaultLogo : `https://sleepercdn.com/avatars/thumbs/${props.avatar}`}
-    alt="new"
-    className="avatar"
-    />
+  <a className = "anchor" href="">
+    <div className="league">
+      <label className="league-name">{props.name}</label>
+      <img
+      src={props.avatar === null ? DefaultLogo : `https://sleepercdn.com/avatars/thumbs/${props.avatar}`}
+      alt="new"
+      className="avatar"
+      />
   </div>
+  </a>
 );
 
 League.propTypes = {};
