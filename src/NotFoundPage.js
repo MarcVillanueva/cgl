a35d7856d395402515e07646a524483015cc6ec2
Marc <marc.villanueva@mitchell.com>
2021-12-18 20:37:17 -0800

    Fix search on NotFoundPage

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -2,11 +2,12 @@ import React from 'react'
 import SearchBar from './components/SearchBar/SearchBar'
 import './styles/NotFoundPage.css'
 import NotFoundImage from './assets/richard_sherman_not_found.png'
+import { useNavigate } from 'react-router-dom';
 
 function NotFoundPage(props) {
   return (
     <div >
-        <SearchBar></SearchBar>
+        <SearchBar navigation={useNavigate()}></SearchBar>
         <img 
         className="not-found-image"
         src={NotFoundImage}
