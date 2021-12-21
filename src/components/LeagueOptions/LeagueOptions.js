5ef11061d40aa5e0bc5b36a1bf6525f41f366eb2
Marc <marc.villanueva@mitchell.com>
2021-12-20 22:03:38 -0800

    Add state for tracking selected league option - Selected option will have a bright green background - Non-selected options will have a dark green background

--- src/components/LeagueOptions/LeagueOptions.js
+++ src/components/LeagueOptions/LeagueOptions.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import styles from './LeagueOptions.css';
+import React, { useState} from 'react';
+import './LeagueOptions.css';
 
-const LeagueOptions = () => (
+const LeagueOptions = props => {
+  const [selectedOption, setSelected] = useState("Rosters");
+
+  return (
   <div className="league-options">
-    <div className="option-button">
+    <div className={selectedOption === "Rosters" ? "option-button-selected" : "option-button"} onClick={() => setSelected("Rosters")}>
       <label >ROSTERS</label>
     </div>
-    <div className="option-button">
+    <div className={selectedOption === "Draft" ? "option-button-selected" : "option-button"} onClick={() => setSelected("Draft")}>
       <label >DRAFT</label>
     </div>
-    <div className="option-button">
+    <div className={selectedOption === "Results" ? "option-button-selected" : "option-button"} onClick={() => setSelected("Results")}>
       <label >RESULTS</label>
     </div>
-    <div className="option-button">
+    <div className={selectedOption === "Collusion" ? "option-button-selected" : "option-button"} onClick={() => setSelected("Collusion")}>
       <label >COLLUSION</label>
     </div>
-    <div className="option-button">
+    <div className={selectedOption === "Weeklys" ? "option-button-selected" : "option-button"} onClick={() => setSelected("Weeklys")}>
       <label >WEEKLYS</label>
     </div>
   </div>
-
-);
+  )
+  };
 
 LeagueOptions.propTypes = {};
 
