f3f9dbda1fa49fe76ff036a8dfe4153c1486783f
Marc <marc.villanueva@mitchell.com>
2021-12-21 16:58:31 -0800

    Add state for tracking 2021 draft of a league

--- src/components/DraftChart/DraftChart.js
+++ src/components/DraftChart/DraftChart.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './DraftChart.css'
 import DraftPick from './../DraftPick/DraftPick'
 
 const DraftChart = (props) => {
-  console.log("League ID: " + props.leagueId)
+  const [draft, setDraft] = useState(null);
+
+  useEffect(() => {
+      async function getDrafts(leagueId, year) {
+        const response = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/drafts`);
+        const draftList = await response.json();
+        var draft = draftList.find(draft => draft.season === year)
+        setDraft(draft);
+      }
+
+    // TODO: Update to take any season instead of hard-coding 2021
+    getDrafts(props.leagueId, "2021");
+  }, [props.leagueId])
+
+  console.log(draft);
   return (
   <div className={styles.DraftChart}>
     <DraftPick></DraftPick>
