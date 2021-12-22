360b8b522a806e2783836ec40df7eb6f846c0b6a
Marc <marc.villanueva@mitchell.com>
2021-12-21 20:45:02 -0800

    Add DraftOrder component - Pass draft_order props from DraftChart to DraftOrder component - Update state in DraftChart to draft instead of draftId so we can utilize the draft state to pass draft_order props to DraftOrder

--- src/components/DraftChart/DraftChart.js
+++ src/components/DraftChart/DraftChart.js
@@ -1,35 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import styles from './DraftChart.css'
 import DraftPick from './../DraftPick/DraftPick'
+import DraftOrder from './../DraftOrder/DraftOrder'
 import { Link } from 'react-router-dom'
 const DraftChart = (props) => {
-  const [draftId, setDraftId] = useState(null);
+  const [draft, setDraft] = useState(null);
   const [draftPicks, setDraftPicks] = useState(null);
 
   useEffect(() => {
-      async function getDraftId(leagueId, year) {
+      async function getDraft(leagueId, year) {
         const response = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/drafts`);
         const draftList = await response.json();
         const draft = draftList.find(draft => draft.season === year)
-        setDraftId(draft.draft_id);
+        setDraft(draft);
       }
     // TODO: Update to take any season instead of hard-coding 2021
-    getDraftId(props.leagueId, "2021");
+    getDraft(props.leagueId, "2021");
   }, [props.leagueId])
 
     useEffect(() => {
       async function getDraftPicks(draftId) {
-        if (draftId) {
-          const response = await fetch(`https://api.sleeper.app/v1/draft/${draftId}/picks`);
+        if (draft?.draft_id) {
+          const response = await fetch(`https://api.sleeper.app/v1/draft/${draft.draft_id}/picks`);
           const draftPicks = await response.json();
           setDraftPicks(draftPicks);
         }
       }
-    getDraftPicks(draftId);
-  }, [draftId])
+    getDraftPicks(draft?.draft_id);
+  }, [draft?.draft_id])
 
   return (
   <div className={styles.DraftChart}>
+      <DraftOrder order={draft?.draft_order}></DraftOrder>
       {draftPicks != null ? 
       draftPicks.map((pick) => (
           <div> 
---
+++ src/components/DraftOrder/DraftOrder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './DraftOrder.css';
+
+const DraftOrder = (props) => (
+  <div className={styles.DraftOrder}>
+    DraftOrder Component
+  </div>
+);
+
+DraftOrder.propTypes = {};
+
+DraftOrder.defaultProps = {};
+
+export default DraftOrder;
