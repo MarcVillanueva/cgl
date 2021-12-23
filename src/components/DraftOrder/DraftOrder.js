5a4cbcf2bd648f6811eb64013ce06cb8eaae3ab5
Marc <marc.villanueva@mitchell.com>
2021-12-22 19:58:09 -0800

    Show sorted user ID, pick order, and draft picks - TODO: Convert user ID to user friendly user name - TODO: Style the lists into a grid/board

--- src/components/DraftOrder/DraftOrder.js
+++ src/components/DraftOrder/DraftOrder.js
@@ -1,11 +1,54 @@
+import { SortByAlphaRounded } from '@material-ui/icons';
 import React from 'react';
 import styles from './DraftOrder.css';
 
-const DraftOrder = (props) => (
-  <div className={styles.DraftOrder}>
-    DraftOrder Component
-  </div>
-);
+const DraftOrder = (props) => {
+  let playerOrderList = [];
+
+  if (props.order) {
+      for (const [playerId, playerOrder] of Object.entries(props.order)) {
+        console.log("playerId:" + playerId)
+        playerOrderList.push(
+          {
+            player_id: playerId,
+            player_order: playerOrder
+          }
+        )
+      }
+  }
+
+  playerOrderList.sort(function(a, b) {
+    return a.player_order - b.player_order;
+  })
+
+  // console.log(sortedPlayerOrder)
+  return(
+    <div>
+      {playerOrderList != null ? 
+      playerOrderList.map((order) => (
+          <div > 
+              <div>
+                <label>{order.player_id}</label>
+                <br></br>
+                <label>{order.player_order}</label>
+              </div>         
+          </div>
+      )) : null}
+    </div>
+  )
+};
+
+
+async function getUsernameById(userId) {
+  if (userId) {
+    const user = await fetch(`https://api.sleeper.app/v1/user/${userId}`);
+    if (user) {
+      console.log("Username: " + user?.username)
+      return user.username
+    }
+  }
+  return null;
+}
 
 DraftOrder.propTypes = {};
 
