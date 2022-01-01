42caf35849b0ab3bf867a6c715caa539242e7f0d
Marc <marc.villanueva@mitchell.com>
2022-01-01 12:33:27 -0800

    Update placement of playoff matchups - Center matchups inside their parent div

--- src/components/PlayoffRound/PlayoffRound.js
+++ src/components/PlayoffRound/PlayoffRound.js
@@ -4,15 +4,18 @@ import PlayoffMatchup from '../PlayoffMatchup/PlayoffMatchup'
 const Matchup = (props) => {
   return (
     <div className="playoff-rounds-panel">
-      {props.matchup !== null ? 
-        <h2 className="playoff-week-header">Week {props.playoffStartWeek + (props.playoffRound - 1)}</h2> : null
-      }
-      {props.matchup != null ? 
-      props.matchup.map((matchup) => (
-          <div className="playoff-round"> 
-            <PlayoffMatchup leagueId={props.leagueId} playoffStartWeek={props.playoffStartWeek} roundNumber={matchup.r} matchup={matchup}></PlayoffMatchup>
+      <div className="playoff-round"> 
+        {props.matchup !== null ? 
+          <h2 className="playoff-week-header">Week {props.playoffStartWeek + (props.playoffRound - 1)}</h2> : null
+        }
+        {props.matchup != null ? 
+        props.matchup.map((matchup) => (
+          <div>
+              <PlayoffMatchup leagueId={props.leagueId} playoffStartWeek={props.playoffStartWeek} roundNumber={matchup.r} matchup={matchup}></PlayoffMatchup>
+              <br></br>
           </div>
-      )) : null}
+        )) : null}
+      </div>
     </div>
   )
 };
