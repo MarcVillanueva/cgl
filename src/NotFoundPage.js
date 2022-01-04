37c53dc5a5134818f384743db059128e1df0a2aa
Marc <marc.villanueva@mitchell.com>
2022-01-03 16:21:56 -0800

    Add image for NotFoundPage - Re-center "User not found" text

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -2,13 +2,16 @@ import React from 'react'
 import SearchBar from './components/SearchBar/SearchBar'
 import './styles/NotFoundPage.css'
 import { useNavigate, useParams } from 'react-router-dom';
-
+import NotFoundImage from './assets/not-found.png'
 function NotFoundPage(props) {
+
   return (
     <div style={{textAlign:"center"}}>
         <SearchBar navigation={useNavigate()} params={useParams()}></SearchBar>
         <br />
         <h1 className="not-found-label">User not found</h1>
+        <img src={NotFoundImage}
+             alt="not-found"></img>
     </div>
   );
 }
